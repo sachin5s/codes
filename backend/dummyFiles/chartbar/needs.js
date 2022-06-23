e0ab175fe11a3beac7f78ac53a7c150eec2a3736
sachin5s <sachinram4968@gmail.com>
2022-06-23 17:53:32 +0530

    bar chart needs

---
+++ backend/dummyFiles/chartbar/needs.js
@@ -0,0 +1,3 @@
+  // needsTOrun  :- 1) install ng2-charts and chart.js
+                //    2) import NgChartModule app.module
+                //    3)  add this script in angular.json  "node_modules/chart.js/dist/chart.js" 
